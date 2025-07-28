171af30cae25c46ba76db1cb2f7612ac9600a67c
igorvlima <igorveraslima3@gmail.com>
2025-07-27 22:51:39 -0300

    change docker image

--- src/test/java/com/example/fiapsoattechchallengecustomerapi/FiapSoatTechChallengeCustomerApiApplicationTests.java
+++ src/test/java/com/example/fiapsoattechchallengecustomerapi/FiapSoatTechChallengeCustomerApiApplicationTests.java
@@ -1,15 +1,17 @@
 package com.example.fiapsoattechchallengecustomerapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EnabledIfSystemProperty(named = "spring.profiles.active", matches = "local|dev|test")
 class FiapSoatTechChallengeCustomerApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        System.out.println("Context loaded successfully in local/dev/test");
+    }
 }
